6ab45b5ac79c18f550f081fe4c8eee56167099f7
fl4meboi <djkim9876@gmail.com>
2024-11-03 14:50:57 -0500

    first commit

---
+++ ProjectTest_02/sketch.js
@@ -0,0 +1,40 @@
+// Slit Scan FX 
+
+let capture;
+const WIDTH = 160
+const HEIGHT = 120
+let images = []
+
+function setup() {
+  createCanvas(WIDTH * 4, HEIGHT * 4);
+  let constraints = {
+    video: {
+      mandatory: {
+        maxWidth: WIDTH,
+        maxHeight: HEIGHT
+      },
+      optional: [{
+        maxFrameRate: 30
+      }]
+    }
+  };
+  capture = createCapture(constraints);
+  capture.size(WIDTH, HEIGHT);
+  capture.hide();
+  background(0)
+  frameRate(30)
+}
+
+function draw() {
+  scale(4)
+  background(0)
+  const g = createGraphics(WIDTH, HEIGHT)
+  g.image(capture, 0, 0, WIDTH, HEIGHT)
+  images.unshift(g)
+  if (images.length > HEIGHT) images.pop()
+  for (let i = 0; i < HEIGHT; i++) {
+    if (images[i]) {
+      image(images[i].get(0, i, WIDTH, 1), 0, i, WIDTH, 1)
+    }
+  }
+}
