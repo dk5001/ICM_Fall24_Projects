e8038b6022d947d3b65e0ecbb2547c6111df2941
fl4meboi <djkim9876@gmail.com>
2024-11-13 17:36:47 -0500

    Prj_07 added

--- ProjectTest_06/sketch.js
+++ ProjectTest_06/sketch.js
@@ -1,7 +1,63 @@
+let img
+let hatch
+let crisp
+let capture
+
 function setup() {
-  createCanvas(400, 400);
+	createCanvas(windowWidth, windowHeight, WEBGL)
+	crisp = createFramebuffer()
+	hatch = createFilterShader(`#version 300 es
+		precision highp float;
+		
+		in vec2 vTexCoord;
+		out vec4 outColor;
+		uniform sampler2D tex0;
+		uniform sampler2D crisp;
+		uniform vec2 canvasSize;
+		uniform float time;
+		
+		vec3 position(vec2 uv) {
+			vec4 color = texture(tex0, uv);
+			float h = (color.r + color.b + color.g) / 3.;
+			return vec3(canvasSize * uv, h * (canvasSize.x + canvasSize.y) * 0.035);
+		}
+		
+		void main() {
+			vec4 orig = texture(crisp, vTexCoord);
+			float brightness = (orig.x + orig.y + orig.z) / 3.;
+			
+			vec3 c = position(vTexCoord);
+
+			outColor = mix(
+				vec4(0., 0., 0., 1.),
+				vec4(1., 1., 1., 1.),
+				smoothstep(
+					0.5,
+					0.51,
+					brightness + smoothstep(0., 0.5, sin(c.z * 4. + time*0.001)) * 0.3
+				)
+			);  
+		}
+	`)
+	
+	capture = createCapture(VIDEO);
+  capture.hide();
 }
 
 function draw() {
-  background(220);
+	crisp.draw(() => {
+		clear()
+		imageMode(CENTER)
+		
+		// Swap commented lines for horse
+		// image(img, 0, 0, width, height, 0, 0, img.width, img.height, COVER, RIGHT, CENTER)
+		image(capture, 0, 0, width, height, 0, 0, capture.width, capture.height, COVER)
+	})
+	clear()
+	imageMode(CENTER)
+	image(crisp, 0, 0)
+	filter(BLUR, 100)
+	hatch.setUniform('crisp', crisp)
+	hatch.setUniform('time', millis())
+	filter(hatch)
 }
